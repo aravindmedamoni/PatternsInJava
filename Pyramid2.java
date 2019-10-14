55677d381f2d4477bb7117f62abab148695c0c72
aravindmedamoni <aravnd.accurate@gmail.com>
2019-10-14 09:29:22 +0530

    another Pyramid pattern code..

---
+++ Pyramid2.java
@@ -0,0 +1,35 @@
+/* write a program to print MirrorRightAngleTrianle
+
+    $
+   $$$
+  $$$$$
+ $$$$$$$
+$$$$$$$$$
+
+*/
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Pyramid2{
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        int n = Integer.parseInt(br.readLine().trim());
+        for(int i=0;i<n;i++){
+            for(int j=n-(i+1);j>0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("$");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+        isr.close();
+        br.close();
+    }
+}
